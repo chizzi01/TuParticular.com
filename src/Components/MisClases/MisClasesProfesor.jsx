70c09cf1d321715ecf212d076795c26d157b0e65
Agustin Chizzini Melo <aguscn2001@gmail.com>
2022-09-29 22:59:51 -0300

    Last changes

--- src/Components/MisClases/MisClasesProfesor.jsx
+++ src/Components/MisClases/MisClasesProfesor.jsx
@@ -1,79 +1,211 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MisClases.css';
-import CardClases from '../Card/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
 
 
 function MisClasesProfesor() {
-            return (
-          <div className="App">
-            <h1>React CRUD App</h1>
-            <h3>Employee List</h3>
-            <div className="form">
-              <input
-                type="text"
-                placeholder="Enter Employee Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Enter Employee Age"
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-              />
-              <input
+  const dataClases = [
 
-                type="text"
-                placeholder="Enter Employee Country"
-                value={country}
-                onChange={(e) => setCountry(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Enter Employee Position"
-                value={position}
-                onChange={(e) => setPosition(e.target.value)}
-              />
-              <input
+    { id: 1, nombre: "Clase 1", descripcion: "Descripcion de la clase 1", duracion: "1 hora", precio: "1000", estado: "Aprobada" },
+    { id: 2, nombre: "Clase 2", descripcion: "Descripcion de la clase 2", duracion: "1 hora", precio: "1000", estado: "Aprobada" },
+  ];
 
-                type="text"
-                placeholder="Enter Employee Wage"
-                value={wage}
-                onChange={(e) => setWage(e.target.value)}
-              />
-              <button onClick={addEmployee}>Add Employee</button>
-            </div>
-            <div className="employees">
-              {employees.map((val, key) => {
-                return (
-                  <div className="employee">
-                    <h3>{val.name}</h3>
-                    <h4>{val.age}</h4>
-                    <h4>{val.country}</h4>
-                    <h4>{val.position}</h4>
-                    <h4>{val.wage}</h4>
-                    <button
-                      onClick={() => {
-                        deleteEmployee(val.id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => {
-                        updateEmployee(val.id);
-                      }}
-                    >
-                      Update
-                    </button>
-                  </div>
-                );
-              }
-              )}
-            </div>
+  const [data, setData] = useState(dataClases);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [claseSeleccionada, setClaseSeleccionada] = useState({
+    id: '',
+    nombre: '',
+    descripcion: '',
+    fecha: '',
+    duracion: '',
+    precio: '',
+    estado: ''
+  });
+
+  const seleccionarClase = (clase, caso) => {
+    setClaseSeleccionada(clase);
+    (caso === "Editar") ? setModalEditar(true) : setModalEliminar(true)
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setClaseSeleccionada((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const editar = () => {
+    var dataNueva = data;
+    dataNueva.map(clase => {
+      if (clase.id === claseSeleccionada.id) {
+        clase.nombre = claseSeleccionada.nombre;
+        clase.descripcion = claseSeleccionada.descripcion;
+        clase.duracion = claseSeleccionada.duracion;
+        clase.precio = claseSeleccionada.precio;
+        clase.estado = claseSeleccionada.estado;
+      }
+    });
+    setData(dataNueva);
+    setModalEditar(false);
+  }
+
+  const eliminar = () => {
+    setData(data.filter(clase => clase.id !== claseSeleccionada.id));
+    setModalEliminar(false);
+  }
+
+  const abrirModalInsertar = () => {
+    setClaseSeleccionada(null);
+    setModalInsertar(true);
+  }
+
+  const insertar = () => {
+    var valorInsertar = claseSeleccionada;
+    valorInsertar.id = data[data.length - 1].id + 1;
+    setData(data.concat(valorInsertar));
+    setModalInsertar(false);
+  }
+
+
+
+  return (
+    <div className='App'>
+      <br />
+      <br />
+      <br />
+      <br />
+      <button className="btn btn-success" onClick={() => abrirModalInsertar()}>Crear</button>
+      <br /><br />
+      <table className='table table-borderer'>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Fecha</th>
+            <th>Duracion</th>
+            <th>Precio</th>
+            <th>Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(clase => (
+            <tr key={clase.id}>
+              <td>{clase.nombre}</td>
+              <td>{clase.descripcion}</td>
+              <td>{clase.fecha}</td>
+              <td>{clase.duracion}</td>
+              <td>{clase.precio}</td>
+              <td>{clase.estado}</td>
+              <td> <button className='btn btn-primary' onClick={() => seleccionarClase(clase, 'Editar')}>Editar</button> {" "}
+                <button className='btn btn-danger' onClick={() => seleccionarClase(clase, 'Eliminar')}>Eliminar</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar Clase</h3>
           </div>
-          );
-          }
+        </ModalHeader>
+        <ModalBody>
+          <div className='form-group'>
+            <label>Id</label>
+            <br />
+            <input type='text' className='form-control' readOnly name='id' value={claseSeleccionada && claseSeleccionada.id} />
+            <br />
+            <label>Nombre</label>
+            <br />
+            <input type='text' className='form-control' name='nombre' value={claseSeleccionada && claseSeleccionada.nombre} onChange={handleChange} />
+            <br />
+            <label>Descripcion</label>
+            <br />
+            <input type='text' className='form-control' name='descripcion' value={claseSeleccionada && claseSeleccionada.descripcion} onChange={handleChange} />
+            <br />
+            <label>Duracion</label>
+            <br />
+            <input type='text' className='form-control' name='duracion' value={claseSeleccionada && claseSeleccionada.duracion} onChange={handleChange} />
+            <br />
+            <label>Precio</label>
+            <br />
+            <input type='text' className='form-control' name='precio' value={claseSeleccionada && claseSeleccionada.precio} onChange={handleChange} />
+            <br />
+            <label>Estado</label>
+            <br />
+            <input type='text' className='form-control' name='estado' value={claseSeleccionada && claseSeleccionada.estado} onChange={handleChange} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-primary' onClick={() => editar()}>Actualizar</button>
+          <button className='btn btn-danger' onClick={() => setModalEditar(false)}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+<Modal isOpen={modalEliminar}>
+        <ModalHeader>
+          <div>
+            <h3>Eliminar Clase</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          Estas seguro que deseas eliminar la clase {claseSeleccionada && claseSeleccionada.nombre}
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-danger' onClick={()=> eliminar()}>Sí</button>
+          <button className='btn btn-secundary' onClick={() => setModalEliminar(false)}>No</button>
+        </ModalFooter>
+</Modal>
+
+<Modal isOpen={modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Crear Clase</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className='form-group'>
+            <label>Id</label>
+            <br />
+            <input type='text' className='form-control' readOnly name='id' value={data[data.length-1].id+1} />
+            <br />
+            <label>Nombre</label>
+            <br />
+            <input type='text' className='form-control' name='nombre' value={claseSeleccionada && claseSeleccionada.nombre} onChange={handleChange} required/>
+            <br />
+            <label>Descripcion</label>
+            <br />
+            <input type='text' className='form-control' name='descripcion' value={claseSeleccionada && claseSeleccionada.descripcion} onChange={handleChange} required/>
+            <br />
+            <label>Duracion</label>
+            <br />
+            <input type='text' className='form-control' name='duracion' value={claseSeleccionada && claseSeleccionada.duracion} onChange={handleChange} required/>
+            <br />
+            <label>Precio</label>
+            <br />
+            <input type='text' className='form-control' name='precio' value={claseSeleccionada && claseSeleccionada.precio} onChange={handleChange} required/>
+            <br />
+            <label>Estado</label>
+            <br />
+            <input type='text' className='form-control' name='estado' value={claseSeleccionada && claseSeleccionada.estado} onChange={handleChange} required/>
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-primary' onClick={() => insertar()}>Crear</button>
+          <button className='btn btn-danger' onClick={() => setModalInsertar(false)}>Cancelar</button>
+        </ModalFooter>
+
+</Modal>
+    </div>
+  );
+}
+
 
 
 export default MisClasesProfesor;
