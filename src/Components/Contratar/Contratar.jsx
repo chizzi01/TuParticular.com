58fc4dfd4198efc7ae54c8a5820526ec0f0d903d
Agustin Chizzini Melo <aguscn2001@gmail.com>
2022-09-30 18:49:58 -0300

    Contratar fix

--- src/Components/Contratar/Contratar.jsx
+++ src/Components/Contratar/Contratar.jsx
@@ -14,12 +14,12 @@ function Recupero() {
             <h1 className='contratar-h1'>Contratar clase</h1>
             <form>
                 <div className='loginForm'>
-                    <h3>Clase: {materia}</h3>
+                    <h3 id='contratar-h3'>Clase: {materia}</h3>
                     <input type='email' placeholder='Correo' required />
                     <input type="number" placeholder='Telefono' required/>
                     <label htmlFor="">Horario de contacto</label>
                     <input type="time" name="" id="" required/>
-                    <textarea name="" id="" cols="30" rows="5" placeholder='Motivo'></textarea>
+                    <textarea name="" id="contratarTextarea" cols="30" rows="5" placeholder='Motivo'></textarea>
                     <input type='submit' value='Contratar' />
                 </div>
             </form>
